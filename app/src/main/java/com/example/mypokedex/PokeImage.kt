3c9a464bd22c38c271edb77ff67f278c78d5c7b0
Butty256 <daiji.suzuki@fuller.co.jp>
2021-04-14 15:51:25 +0900

    不要なファイルの削除

--- app/src/main/java/com/example/mypokedex/PokeImage.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mypokedex
-
-class PokeImage {
-    var id: Int = 0
-    var name: String = "Missing No."
-    var url: String = "https://1.bp.blogspot.com/-W895RS_ucM0/VwIjSh3myrI/AAAAAAAA5h0/l_lsBqjMh0ImiZyBJb10hB28CW4eJRYFg/s800/tv_sunaarashi.png"
-}
