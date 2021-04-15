640764e291ac59c6031fafa02e161d1943df54dc
Butty256 <daiji.suzuki@fuller.co.jp>
2021-04-15 18:25:44 +0900

    結局解決できてない

--- app/src/main/java/com/example/mypokedex/DetailsFragment.kt
+++ app/src/main/java/com/example/mypokedex/DetailsFragment.kt
@@ -7,13 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.mypokedex.databinding.FragmentDetailsBinding
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.concurrent.thread
 
 class DetailsFragment: Fragment() {
+
+    val detailsViewModel = DetailsViewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,31 +31,28 @@ class DetailsFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val args: DetailsFragmentArgs by navArgs()
-        val info: PokeInfo = getData(args.entry)
 
-        val idView: TextView = view.findViewById(R.id.id_detail_text)
-        val nameView: TextView = view.findViewById(R.id.name_detail_text)
-        val flavorView: TextView = view.findViewById(R.id.flavor_detail_text)
+        val binding: FragmentDetailsBinding = DataBindingUtil.bind(view)?: throw RuntimeException("FragmentDetailsBinding not done.")
+        binding.viewModel = detailsViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
         val iconView: ImageView = view.findViewById(R.id.icon_detail_image)
-        idView.text = info.id.toString()
-        nameView.text = info.name.capitalize()
-
-        // どうにかしたい
-        val flavorSize: Int = info.flavor_text_entries.size
-        for (i in 0..flavorSize) {
-            if (info.flavor_text_entries[i].language == Language("en") &&
-                info.flavor_text_entries[i].version == Version("red")) {
-                flavorView.text = info.flavor_text_entries[i].flavor_text
-                break
-            }
-        }
 
         GlideApp.with(iconView)
             .load(
                 "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-                        info.id.toString() + ".png"
+                        args.entry.toString() + ".png"
             )
             .into(iconView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        //val data: PokeInfo = getData(args.entry)
+        //println(data.name)
+        //detailsViewModel.setData2(data)
+        detailsViewModel.setData(5)
     }
 
     private fun getData(id: Int): PokeInfo {
@@ -72,4 +74,5 @@ class DetailsFragment: Fragment() {
         Thread.sleep(1000)
         return data
     }
+
 }
---
+++ app/src/main/java/com/example/mypokedex/DetailsViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.mypokedex
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.concurrent.thread
+
+class DetailsViewModel: ViewModel() {
+    val idText = MutableLiveData<String>("0")
+    val nameText = MutableLiveData<String>("Missing No.")
+    val flavorText = MutableLiveData<String>("")
+
+    fun setData(id: Int) {
+        viewModelScope.launch {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://pokeapi.co/api/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            var data: PokeInfo = PokeInfo()
+            thread {
+                try {
+                    val service: PokeInfoService = retrofit.create(PokeInfoService::class.java)
+                    data = service.pokeInfo(id).execute().body() ?: throw IllegalStateException("NULL")
+                } catch (e: Exception) {
+                    Log.d("api", "debug $e")
+                }
+            }
+            delay(500)
+            //while (data.id == 0) delay(1)
+            println(data.id)
+            idText.value = data.id.toString()
+            nameText.value = data.name
+            val flavorSize: Int = data.flavor_text_entries.size
+            for (i in 0..flavorSize) {
+                if (data.flavor_text_entries[i].language == Language("en") &&
+                    data.flavor_text_entries[i].version == Version("red")) {
+                    flavorText.value = data.flavor_text_entries[i].flavor_text
+                    break
+                }
+            }
+        }
+    }
+
+    fun setData2(data: PokeInfo) {
+        idText.value = data.id.toString()
+        nameText.value = data.name.capitalize()
+        flavorText.value = data.flavor_text_entries[0].flavor_text
+    }
+}
